ed43e7e69170574651a04869838e6bf7ea1ef8b6
frijo <frijo45@gmail.com>
2014-04-01 00:33:24 -0600

    Version 2
    Version 2

---
+++ BLOG/BLOG/Controllers/AdminFrontendController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BLOG.Controllers
+{
+    public class AdminFrontendController : Controller
+    {
+        //
+        // GET: /AdminFrontend/
+
+        public ActionResult AdmFrontend()
+        {
+            return View();
+        }
+
+    }
+}
