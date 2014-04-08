30675bba607097d23b19b41712688d28617c25ab
frijo <frijo45@gmail.com>
2014-04-08 17:35:12 -0600

    version 4
    version 4

---
+++ BLOG/BLOG/Models/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BLOG.Models
+{
+    public class Admin
+    {
+        public String Name { get; set; }
+        public String Biografy { get; set; }
+        public String SocialNetworks { get; set; }
+    }
+}
