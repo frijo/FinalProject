ee05202de1d944be70586ed95007909af2d789dd
frijo <frijo45@gmail.com>
2014-04-08 20:50:54 -0600

    version 5
    version 5

---
+++ BLOG/BLOG/Models/Administrador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BLOG.Models
+{
+    public class Administrador
+    {
+        public String user { get; set; }
+        public String password { get; set; }
+
+    }
+}
