a088964f0fee8fa8373bf636d31c9666050b7a2f
frijo <frijo45@gmail.com>
2014-04-19 23:15:38 -0600

    version11
    v11

--- BLOG/BLOG/Controllers/AcountController.cs
+++ BLOG/BLOG/Controllers/AcountController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data;
+using System.Security.Cryptography;
+using System.Web.Security;
 
 namespace BLOG.Controllers
 {
@@ -21,27 +23,79 @@ namespace BLOG.Controllers
         [HttpPost]
         public ActionResult Create(String NickName="",String Password="",String Name="", String SN1="",String SN2="",String Ocupation="")
         {
-            //String passwordEncrip = Helper.EncodePassword(string.Concat(usuario.NombreLogin, usuario.Password));
+             
+            var passCryp = new SimpleCrypto.PBKDF2();
+            var crypPass = passCryp.Compute(Password);
+
+
             Blogger nuevo = new Blogger();
             nuevo.NickName = NickName;
-            nuevo.password = Password;
+            nuevo.password = crypPass;
+            nuevo.passwordSalt = passCryp.Salt;
             nuevo.Name = Name;
             nuevo.Ocupacion = Ocupation;
             nuevo.SocialNet1 = SN1;
             nuevo.SocialNet2 = SN2;
 
-            if (ModelState.IsValid)
-            {
-                db.Bloggers.Add(nuevo);
-                db.SaveChanges();
-                return RedirectToAction("Login");
-            }
+            
+                if (ModelState.IsValid)
+                {
+                    
+                        db.Bloggers.Add(nuevo);
+                        db.SaveChanges();
+                    
+                   
+                    return RedirectToAction("Login");
+                }
+            
             return View();
         }
+        
         public ActionResult Login()
         {
             return View();
         }
+        [HttpPost]
+
+
+        public ActionResult Login(String LoginUser = "", String LoginPass = "")
+        {
+            if (ModelState.IsValid)
+            {
+                if (IsValid(LoginUser, LoginPass))
+                {
+                    FormsAuthentication.SetAuthCookie(LoginUser, false);
+                    return RedirectToAction("Index", "Admin");
+
+                }
+                else 
+                {
+                    ModelState.AddModelError(""," Login Data Incorrect");
+                }
+            }
+            return View();
+        }
+
+
+        private bool IsValid(String NickName, String Password)
+        {
+            bool Valid = false;
+            var passCryp = new SimpleCrypto.PBKDF2();
+
+            using (var data = new Application_Db())
+            {
+                var user = data.Bloggers.FirstOrDefault(n => n.NickName == NickName);
+                if (user != null)
+                {
+                    if (user.password == passCryp.Compute(Password, user.passwordSalt))
+                    {
+                        Valid = true;
+                    }
+                }
+            }
+            return Valid;
+        }
+
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
--- BLOG/BLOG/Models/Blogger.cs
+++ BLOG/BLOG/Models/Blogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,17 @@ namespace BLOG.Models
 {
     public class Blogger
     {
+        [Required]
         public int ID { get; set; }
+        [Required]
         public String NickName { get; set; }
+        [Required]
         public String password { get; set; }
+        [Required]
+        public String passwordSalt { get; set; }
+        [Required]
         public String Name { get; set; }
+        
         public String Ocupacion { get; set; }
         public String SocialNet1 { get; set; }
         public String SocialNet2 { get; set; }
